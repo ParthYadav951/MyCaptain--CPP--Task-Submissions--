4a84a694ba132b96e6517050a878f7dd15ba6371
ParthYadav951 <83538277+ParthYadav951@users.noreply.github.com>
2021-08-24 16:28:10 +0530

    Create Task2-Let's Code! : Decision Making.cpp

---
+++ Task2-Let's Code! : Decision Making.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    cout << "Enter age of person:";
+    cin>>age;
+
+    if (age >= 18) 
+    {
+        cout << "You are eligible for voting";
+    } 
+    else 
+    {
+        cout << "You are not eligible for voting";
+    }
+
+    return 0;
+    
+    // Code....
+    // Written....
+    // By....
+    // Parth....
+    // Yadav....
+
+}
